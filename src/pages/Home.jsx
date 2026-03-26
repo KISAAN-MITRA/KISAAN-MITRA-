d5a6a5a042ee1037b1d9b4cbd3b7bb1b921813e0
Prathvi321 <prathvisinght3@gmail.com>
2026-03-26 21:34:14 +0530

    New Product Logo & Interchange of the places of some Divs on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,8 +22,8 @@ const Home = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAxMCAwIEwgMCAwIDAgMTAiIGZpbGw9Im5vbmUiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMSIvPjwvcGF0dGVybj48L2RlZnM+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgZmlsbD0idXJsKCNncmlkKSIvPjwvc3ZnPg==')]"></div>
       </div>
       <Hero />
-      <WhatIsThis />
       <ValueMetrics />
+      <WhatIsThis />
       <Testimonials />
       <Impact />
       <Gen2Vision />
